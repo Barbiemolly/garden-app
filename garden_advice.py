eafcd5aa7c79d9c1cd239c8aa70d7bab67fe54fe
Molemo Khethwa <123152642+Barbiemolly@users.noreply.github.com>
2025-06-30 23:06:01 +0200

    Initail commit

---
+++ garden_advice.py
@@ -0,0 +1,11 @@
+# TODO: Create a function for getting gardening tips based on month
+# TODO: Replace hardcoded values with a data dictionary
+# TODO: Add docstrings for all functions
+
+month = "June"
+if month == "June":
+    print("Protect your plants from frost.")
+elif month == "December":
+    print("Water your plants early in the morning.")
+else:
+    print("Check your local gardening calendar.")
